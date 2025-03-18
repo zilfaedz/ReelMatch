f06d5e6346ff1945ff2f27e010dba946574e6b00
zilfaedz <zilfaedz.quirante@cit.edu>
2025-03-18 18:05:27 +0800

    Initial Commit

---
+++ app/src/main/java/com/android/reelmatch/ProfileSettingsPage.kt
@@ -0,0 +1,20 @@
+package com.android.reelmatch
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ProfileSettingsPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_profile_settings_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
